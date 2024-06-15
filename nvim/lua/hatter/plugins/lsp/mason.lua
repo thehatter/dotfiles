8b9cc0a8915f041ac81ed790eca62d3abd7318a2
Andrei Liotenko <andrewlutenko@gmail.com>
2024-06-15 15:43:51 +0300

    feat: add new plugins

--- nvim/lua/hatter/plugins/lsp/mason.lua
+++ nvim/lua/hatter/plugins/lsp/mason.lua
@@ -35,6 +35,7 @@ return {
         "emmet_ls",
         "pyright",
         "solargraph",
+        "gopls"
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
