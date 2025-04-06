af5496088bf844d4abfecc3e876ec60248dd9a3f
Andrei Liotenko <andrewlutenko@gmail.com>
2025-04-06 22:24:44 +0100

    feat: configure snacks terminal

--- nvim/lua/hatter/plugins/snacks.lua
+++ nvim/lua/hatter/plugins/snacks.lua
@@ -1,7 +1,7 @@
 return {
   "folke/snacks.nvim",
   priority = 1000,
- lazy = false,
+  lazy = false,
   ---@type snacks.Config
   opts = {
     -- your configuration comes here
@@ -22,10 +22,19 @@ return {
       -- your zen configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+    },
+    terminal = {
+      win = {
+        keys = {
+          q = { "<C-q>", function() Snacks.terminal.toggle() end, desc = "Togglq terminal", expr = true, mode = "t" },
+          -- related config example https://github.com/jellydn/my-nvim-ide/blob/main/lua/plugins/extras/snacks.lua
+        },
+      }
     }
   },
   keys = {
     { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+    { "<leader>T",  function() Snacks.terminal.toggle() end, desc = "Toggle Terminal!" },
   }
 }
